da2a95c2fcd47884450b8db4438d7a0b52ef5f44
Ethan Haller <2150020@lfschools.net>
2020-09-09 18:56:30 +0000

    Implement Stack into the ArrayStack method

--- ArrayStack.java
+++ ArrayStack.java
@@ -4,7 +4,38 @@
  *
  * @author (your name)
  */
-public class ArrayStack
-{
+public class ArrayStack<E> implements Stack<E> {
+  Object[] stack = new Object[100];
 
+  public Object[] s = new Object[100];
+
+  public E pop(){
+    E ret = s[0]
+    for(int i=0; i<s.length-1; i++){
+      s[i] = s[i+1];
+    }
+    return ret;
+  }
+
+  public void push(E p){
+    if(s[99]!==null){
+      System.out.println("ERROR!!!");
+    }
+    for(int i = s.length-1; i<0; i--){
+      s[i] = s[i-1];
+    }
+    s[0] = p;
+  }
+
+  public E peekTop(){
+    return s[0];
+  }
+
+  public boolean isEmpty(){
+    for(int i=0; i<s.length; i++){
+      if(s[i]!=null){
+        return false;
+      }
+  }
+  return true;
 }
