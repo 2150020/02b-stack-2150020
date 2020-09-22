3c0e88865e78e0815555c4b89c5a619c29fc249c
2150020 <69986924+2150020@users.noreply.github.com>
2020-09-21 22:04:09 -0500

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * ArrayStack should implement the Stack interface. You should write your own
  * test cases in ArrayStackTest
@@ -5,30 +6,43 @@
  * @author (your name)
  */
 public class ArrayStack<E> implements Stack<E> {
-  Object[] stack = new Object[100];
 
   public Object[] s = new Object[100];
 
   public E pop() {
-    E ret = (E)(s[0]);
-    for (int i = 0; i < s.length - 1; i++) {
-      s[i] = s[i + 1];
+    E ret = null;
+    if (isEmpty()) {
+      throw new StackException();
+    } 
+    else {
+      ret = (E) (s[0]);
+      for (int i = 0; i < s.length - 1; i++) {
+        s[i] = s[i + 1];
+      }
     }
     return ret;
   }
 
   public void push(E p) {
-    if (s[99] != null) {
-      System.out.println("ERROR!!!");
+    if (s[s.length-1] != null) {
+      Object[] n = new Object[s.length+1];
+      for(int i=0; i<s.length;i++){
+        n[i] = s[i];
+      }
     }
-    for (int i = s.length - 1; i < 0; i--) {
+    for (int i = s.length - 1; i > 0; i--) {
       s[i] = s[i - 1];
     }
     s[0] = p;
   }
 
   public E peekTop() {
-    return (E)(s[0]);
+    if(isEmpty()){
+      throw new StackException();
+    }
+    else{
+    return (E) (s[0]);
+    }
   }
 
   public boolean isEmpty() {
